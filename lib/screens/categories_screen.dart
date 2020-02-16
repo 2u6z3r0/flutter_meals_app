95f587aafa81f166af6ed7186e75e8d69488fa1f
2u6z3r0 <samrat.patil0202@gmail.com>
2020-02-16 13:10:27 +0530

    house keeping - organised files in better way

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './dummy_data.dart';
-import './category_item.dart';
+import '../dummy_data.dart';
+import '../widgets/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
@@ -19,7 +19,8 @@ class CategoriesScreen extends StatelessWidget {
           mainAxisSpacing: 20,
         ),
         children: DUMMY_CATEGORIES
-            .map((catData) => CategoryItem(catData.id, catData.title, catData.bgColor))
+            .map((catData) =>
+                CategoryItem(catData.id, catData.title, catData.bgColor))
             .toList(),
       ),
     );
